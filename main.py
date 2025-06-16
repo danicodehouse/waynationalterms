0ebaeb27768c843c15a2838867bcb0fbfa14b3a4
danicodehouse <119807915+danicodehouse@users.noreply.github.com>
2025-06-16 21:48:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def generate_captcha_image(code):
     # Convert the image to base64 string to pass to the HTML
     return base64.b64encode(img_io.getvalue()).decode('utf-8')
 
-@app.route('/m', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def captcha():
     if request.method == 'GET':
         if 'passed_captcha' in session and session['passed_captcha']:
@@ -213,7 +213,7 @@ def success():
         return redirect(url_for('captcha'))
 
 
-@app.route("/")
+@app.route("/m")
 def route2():
     web_param = request.args.get('web')
     if web_param:
